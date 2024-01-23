0b8b51344d284680408f7d6938e6bd8b41e294c3
monoramasn <146131219+monoramasn@users.noreply.github.com>
2024-01-23 18:25:51 +0100

    Update English.py

--- English.py
+++ English.py
@@ -13,9 +13,9 @@ from transformers import WhisperFeatureExtractor, WhisperTokenizer, WhisperProce
 
 from datasets import load_dataset, load_metric
 
-En_voxpopuli_dataset = load_dataset("facebook/voxpopuli", "En", split=['train', 'test'])
+en_voxpopuli_dataset = load_dataset("facebook/voxpopuli", "en", split=['train', 'test'])
 
-dataset = En_voxpopuli_dataset.remove_columns(['audio_id', 'language', 'raw_text', 'gender', 'speaker_id', 'is_gold_transcript', 'accent'])
+dataset = en_voxpopuli_dataset.remove_columns(['audio_id', 'language', 'raw_text', 'gender', 'speaker_id', 'is_gold_transcript', 'accent'])
 
 
 #dataset
@@ -32,8 +32,8 @@ normalizer = BasicTextNormalizer()
 
 model_checkpoint= "openai/whisper-large"
 feature_extractor = WhisperFeatureExtractor.from_pretrained(model_checkpoint)
-tokenizer = WhisperTokenizer.from_pretrained(model_checkpoint, language="German", task="transcribe")
-processor = WhisperProcessor.from_pretrained(model_checkpoint, language="German", task="transcribe")
+tokenizer = WhisperTokenizer.from_pretrained(model_checkpoint, language="English", task="transcribe")
+processor = WhisperProcessor.from_pretrained(model_checkpoint, language="English", task="transcribe")
 model = WhisperForConditionalGeneration.from_pretrained(model_checkpoint)
 
 if model.config.decoder_start_token_id is None:
