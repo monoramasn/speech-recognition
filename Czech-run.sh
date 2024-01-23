edb54993e4747b20fa14331829707e2cc57576e2
monoramasn <146131219+monoramasn@users.noreply.github.com>
2024-01-23 13:41:20 +0530

    Update Czech-run.sh

--- Czech-run.sh
+++ Czech-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=tiny
+#SBATCH --job-name=large
 #SBATCH -p gpu --gres=gpu:a100:1
 #SBATCH --time=11:30:00
-#SBATCH --output=logs/tiny.log
+#SBATCH --output=logs/large.log
 #SBATCH --ntasks=1 --cpus-per-task=8 --mem=16GB
 echo $SLURMD_NODENAME $CUDA_VISIBLE_DEVICES
 . /etc/profile.d/modules.sh
@@ -11,7 +11,7 @@ nvidia-smi
 conda activate /home/nkx870/anaconda3/envs/monorama
 
 # ro de en fr pl it es
-for language in et
+for language in cs
 do
-    python3.9 program.py --language $language --model_size tiny
+    python3.9 program.py --language $language --model_size large
 done
