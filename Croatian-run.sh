8a1e3c09e3f5c8c90ee681c0e15d8f86c04b09b8
monoramasn <146131219+monoramasn@users.noreply.github.com>
2024-01-24 10:19:45 +0100

    Update Croatian-run.sh

--- Croatian-run.sh
+++ Croatian-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=large
+# SBATCH --cpus-per-task=3 --mem=2048000M
+# SBATCH --time=00:05:00
+#SBATCH -p gpu --gres=gpu:4
 #SBATCH -p gpu --gres=gpu:a100:1
-#SBATCH --time=11:30:00
 #SBATCH --output=logs/large.log
 #SBATCH --ntasks=1 --cpus-per-task=8 --mem=32GB
 echo $SLURMD_NODENAME $CUDA_VISIBLE_DEVICES
