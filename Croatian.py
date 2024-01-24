83a5eef6e2e7b513ed72607a90955a622594e2a9
monoramasn <146131219+monoramasn@users.noreply.github.com>
2024-01-24 18:23:32 +0100

    Update Croatian.py

--- Croatian.py
+++ Croatian.py
@@ -310,7 +310,7 @@ from transformers import TrainingArguments
 training_args = Seq2SeqTrainingArguments(
   output_dir=repo_name,
   group_by_length=True,
-  per_device_train_batch_size=4,
+  per_device_train_batch_size=8,
   evaluation_strategy="steps",
   num_train_epochs=50,
   #fp16=False,
