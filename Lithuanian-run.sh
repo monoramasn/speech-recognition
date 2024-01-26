3f9e300018fb7eca2fb18c735ec446b33d6655fa
monoramasn <146131219+monoramasn@users.noreply.github.com>
2024-01-26 10:09:47 +0100

    Update Lithuanian-run.sh

--- Lithuanian-run.sh
+++ Lithuanian-run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=large
-#SBATCH -p gpu --gres=gpu:a100:1
-#SBATCH --time=11:30:00
+#SBATCH --time=1-00:00:00
+#SBATCH -p gpu --gres=gpu:a100:2
 #SBATCH --output=logs/large.log
-#SBATCH --ntasks=1 --cpus-per-task=8 --mem=16GB
+#SBATCH --ntasks=1 --cpus-per-task=8 --mem=32GB
 echo $SLURMD_NODENAME $CUDA_VISIBLE_DEVICES
 . /etc/profile.d/modules.sh
 eval "$(conda shell.bash hook)"
 nvidia-smi
 conda activate /home/nkx870/anaconda3/envs/monorama
 
-# ro de en fr pl it es
+# ro de en fr pl it 
 for language in lt
 do
     python3.9 Lithuanian.py --language $language --model_size large
